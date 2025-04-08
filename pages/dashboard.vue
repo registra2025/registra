5921a2bdda997b8a39ce3bebe1bf3586531cc272
Woodiewood_2130 <mohddawood2002@gmail.com>
2025-04-08 06:11:44 +0300

    fix

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -5,15 +5,15 @@ definePageMeta({ middleware: "auth" });
 <template>
   <div>
       <!-- <h1>Home Page Content</h1> -->
-  <div class="flex flex-row justify-between p-10">
+ <div class="flex flex-row justify-between p-10">
     <div class="w-full max-w-[258px] h-[135px] bg-[#0f243d] rounded-[7px] flex items-center justify-center">
-      <Nuxtlink to="/purchase" class="text-white text-[32px]">PURCHASE</Nuxtlink>
+      <NuxtLink to="/purchase" class="text-white text-[32px]">PURCHASE</NuxtLink>
     </div>
     <div class="w-full max-w-[258px] h-[135px] bg-[#0f243d] rounded-[7px] flex items-center justify-center">
-      <Nuxtlink to="/sales" class="text-white text-[32px]">SALES</Nuxtlink>
+      <NuxtLink to="/sales" class="text-white text-[32px]">SALES</NuxtLink>
     </div>
     <div class="w-full max-w-[258px] h-[135px] bg-[#0f243d] rounded-[7px] flex items-center justify-center">
-      <Nuxtlink to="/scan" class="text-white text-[32px]">SCAN</Nuxtlink>
+      <NuxtLink to="/scan" class="text-white text-[32px]">SCAN</NuxtLink>
     </div>
   </div>
     </div>
