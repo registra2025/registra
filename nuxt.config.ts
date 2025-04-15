53b5852769a544b290a3a23020906c498ec49cd7
Woodiewood_2130 <mohddawood2002@gmail.com>
2025-04-16 02:02:37 +0300

    update to nuxt.config
    for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
+   //  ssr: true,
     modules: ['@pinia/nuxt'],
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
