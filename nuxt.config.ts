c9eb83b7a98d26a2cd98088a81481907175ff085
Woodiewood2130 <mohddawood2002@gmail.com>
2026-01-23 14:06:06 +0300

    v3.8.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-   //  ssr: true,
     modules: ['@pinia/nuxt'],
     compatibilityDate: '2024-11-01',
     vue: {
-      compilerOptions: {
-        isCustomElement: tag => tag === 'marquee'
-      }
+        compilerOptions: {
+            isCustomElement: tag => tag === 'marquee'
+        }
     },
-    // devtools: { enabled: true },
-    // pages: false,
     css: ['@/assets/css/main.css'],
     app: {
-      head: {
-        link: [
-          { rel: 'icon', type: 'image/png', href: '/reg_logo_rm.png' }
-        ]
-      }
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/png', href: '/reg_logo_rm.png' }
+            ]
+        }
     },
     vite: {
         plugins: [
-            tailwindcss(),
+            tailwindcss()
         ]
-    },
-    // ssr: false
+    }
 });
